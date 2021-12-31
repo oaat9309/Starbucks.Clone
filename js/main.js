42a9df28ba02478ef968eeeabacc6cde935477b9
oaat9309 <kite1993@naver.com>
2021-12-31 20:39:32 +0900

    (12.31) Badge & Visual
    - Badge 요소 완성 (gsap, lodash 활용)
    - Visual 요소 이미지 배치
    - btn class 정의
    
    Happy New Year!

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ searchInput.addEventListener('blur', () => {
   searchEl.classList.remove('focused')
   searchInput.setAttribute('placeholder', '')
 })
+
+/* Badge */
+
+const badgeEl = document.querySelector('.badges')
+
+window.addEventListener('scroll', _.throttle(function() {
+  if(window.scrollY > 500) {
+    // gsap.to(요소, 시간, 옵션)
+    gsap.to(badgeEl, .3, {
+      opacity: 0,
+      display: 'none'
+    })
+  }
+  else {
+    gsap.to(badgeEl, .3, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300)) 
+// _.throttle(함수, 시간)
