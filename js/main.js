f6a712f0361321ba4fcecc265a5b95b81117ef3d
oaat9309 <kite1993@naver.com>
2022-01-01 18:20:57 +0900

    (01.01) Notice & Promotion
    - Notice 요소 완성
    - Promotion 요소 완성(swiper css 적용)
    - Promotion toggel btn 완성

--- js/main.js
+++ js/main.js
@@ -35,3 +35,58 @@ window.addEventListener('scroll', _.throttle(function() {
   }
 }, 300)) 
 // _.throttle(함수, 시간)
+
+
+/* Visual Animation */
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  })
+})
+
+/* Swiper Css */
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+})
+
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, 
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 3000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev', 
+    nextEl: '.promotion .swiper-next'
+  }
+})
+
+
+/* Toggle Promotion */
+
+const promotionEl = document.querySelector('.promotion')
+const promotionToggelBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false
+
+promotionToggelBtn.addEventListener('click', () => {
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion) {
+    promotionEl.classList.add('hide')
+  }
+  else {
+    promotionEl.classList.remove('hide')
+  }
+})
